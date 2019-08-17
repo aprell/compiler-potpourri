7c342ae2a750c671308c68050392faa0b1ce8799
Andreas Prell <andreas.h.prell@gmail.com>
2019-08-17 15:48:55 +0200

    Add support for backward data flow problems

--- data_flow/analysis/data_flow.ml
+++ data_flow/analysis/data_flow.ml
@@ -29,7 +29,7 @@ let dfs_postorder (graph : cfg) =
   dfs_reverse_postorder graph
   |> List.rev
 
-(* Input signatures of functor Forward_flow *)
+(* Input signatures of functors Forward_flow and Backward_flow *)
 module type SetType = sig
   include Set.S
 end
@@ -40,12 +40,13 @@ module type AnalysisType = sig
   val init : node -> cfg -> t analysis
 end
 
-(* Output signature of functor Forward_flow
+(* Output signature of functors Forward_flow and Backward_flow
    Input signature of functor Data_flow_analysis *)
 module type DataFlowType = sig
   type t
   val traverse : cfg -> Nodes.elt list
   val init : cfg -> t analysis array
+  (* Update in-set and out-set of a node *)
   val update : node -> t analysis array -> t * t
 end
 
@@ -58,7 +59,6 @@ module Forward_flow (S : SetType) (T : AnalysisType with type t := S.t) = struct
     let num_nodes = Array.length graph in
     Array.init num_nodes (fun i -> T.init graph.(i) graph)
 
-  (* Update in-set and out-set of a node *)
   let update (n : node) sets =
     let { index = i; block = Basic_block (name, _); pred; _ } = n in
     let { gen; kill; global_in; _ } = sets.(i) in
@@ -71,6 +71,31 @@ module Forward_flow (S : SetType) (T : AnalysisType with type t := S.t) = struct
           ) pred (sets.(Nodes.choose pred).global_out)
         in
         let out_set = S.union gen (S.diff in_set kill) in
+        (in_set, out_set)
+      )
+end
+
+module Backward_flow (S : SetType) (T : AnalysisType with type t := S.t) = struct
+  type t = S.t
+
+  let traverse = dfs_postorder
+
+  let init graph =
+    let num_nodes = Array.length graph in
+    Array.init num_nodes (fun i -> T.init graph.(i) graph)
+
+  let update (n : node) sets =
+    let { index = i; block = Basic_block (name, _); succ; _ } = n in
+    let { gen; kill; global_out; _ } = sets.(i) in
+    match name with
+    | "Exit" -> (S.union gen global_out, global_out)
+    | _ -> (
+        assert (Nodes.cardinal succ > 0);
+        let out_set = Nodes.fold (fun s set ->
+            T.meet sets.(s).global_in set
+          ) succ (sets.(Nodes.choose succ).global_in)
+        in
+        let in_set = S.union gen (S.diff out_set kill) in
         (in_set, out_set)
       )
 end
