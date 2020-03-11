35c90ef88c0b9f0bfd21110c25a3670e71709675
Andreas Prell <andreas.h.prell@gmail.com>
2020-03-12 00:04:40 +0100

    Reduce code duplication

--- data_dependences/analysis/test.lua
+++ data_dependences/analysis/test.lua
@@ -1,52 +1,54 @@
 local ArrayAccess = require "array_access"
 local Dependence = require "dependence"
 
-local result = Dependence.analyze(
-    ArrayAccess "A[i + 1][j + 2][k + 3]",
-    ArrayAccess "A[i][j][k]")
+local function test(t)
+    local result = Dependence.analyze(
+        ArrayAccess(t.input[1]),
+        ArrayAccess(t.input[2]))
 
-for i, s in ipairs(result) do
-    print(i, s.subscript[1], s.subscript[2], s.variables, s.class, s.coupled)
+    for i, s in ipairs(result) do
+        print(i, s.subscript[1], s.subscript[2], s.variables, s.class, s.coupled)
+    end
+
+    assert(Dependence.test(result) == t.expect)
 end
 
-assert(Dependence.test(result) == true)
+test {
+    input = {
+        "A[i + 1][j + 2][k + 3]",
+        "A[i][j][k]"
+    },
+    expect = true
+}
 
-local result = Dependence.analyze(
-    ArrayAccess "A[5][i + 1][j]",
-    ArrayAccess "A[10][i][k]")
+test {
+    input = {
+        "A[5][i + 1][j]",
+        "A[10][i][k]"
+    },
+    expect = false
+}
 
-for i, s in ipairs(result) do
-    print(i, s.subscript[1], s.subscript[2], s.variables, s.class, s.coupled)
-end
+test {
+    input = {
+        "A[i][j][j]",
+        "A[i][j][k]"
+    },
+    expect = true
+}
 
-assert(Dependence.test(result) == false)
+test {
+    input = {
+        "A[2*i + 2]",
+        "A[2*i + 1]"
+    },
+    expect = true
+}
 
-local result = Dependence.analyze(
-    ArrayAccess "A[i][j][j]",
-    ArrayAccess "A[i][j][k]")
-
-for i, s in ipairs(result) do
-    print(i, s.subscript[1], s.subscript[2], s.variables, s.class, s.coupled)
-end
-
-assert(Dependence.test(result) == true)
-
-local result = Dependence.analyze(
-    ArrayAccess "A[2*i + 2]",
-    ArrayAccess "A[2*i + 1]")
-
-for i, s in ipairs(result) do
-    print(i, s.subscript[1], s.subscript[2], s.variables, s.class, s.coupled)
-end
-
-assert(Dependence.test(result) == true)
-
-local result = Dependence.analyze(
-    ArrayAccess "A[3*i]",
-    ArrayAccess "A[2*i + 1]")
-
-for i, s in ipairs(result) do
-    print(i, s.subscript[1], s.subscript[2], s.variables, s.class, s.coupled)
-end
-
-assert(Dependence.test(result) == true)
+test {
+    input = {
+        "A[3*i]",
+        "A[2*i + 1]"
+    },
+    expect = true
+}
