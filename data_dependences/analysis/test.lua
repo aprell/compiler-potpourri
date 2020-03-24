31ed1e9c4d0738d4b24e9c81b6e5176e2948aab8
Andreas Prell <andreas.h.prell@gmail.com>
2020-03-24 18:45:53 +0100

    Update tests

--- data_dependences/analysis/test.lua
+++ data_dependences/analysis/test.lua
@@ -1,3 +1,7 @@
+package.path = "../../utils/?.lua;" .. package.path
+
+local Set = require "set"
+
 local ArrayAccess = require "array_access"
 local Dependence = require "dependence"
 
@@ -6,35 +10,30 @@ local function test(t)
         ArrayAccess(t.input[1]),
         ArrayAccess(t.input[2]))
 
-    for i, s in ipairs(result) do
-        print(i, s.subscript[1], s.subscript[2], s.variables, s.class, s.coupled)
+    for dim, expect in ipairs(t.expect) do
+        assert(result[dim].variables == expect.variables)
+        assert(result[dim].class == expect.class)
+        assert(result[dim].coupled == expect.coupled)
     end
 
-    assert(Dependence.test(result) == t.expect)
+    assert(Dependence.test(result) == t.dependence)
 end
 
-test {
-    input = {
-        "A[i + 1][j + 2][k + 3]",
-        "A[i][j][k]"
-    },
-    expect = true
-}
+-- Separable subscripts
 
 test {
     input = {
-        "A[5][i + 1][j]",
-        "A[10][i][k]"
+        "A[i]",
+        "A[i-1]"
     },
-    expect = false
-}
-
-test {
-    input = {
-        "A[i][j][j]",
-        "A[i][j][k]"
+    expect = {
+        {
+            variables = Set {"i"},
+            class = "SIV",
+            coupled = false
+        }
     },
-    expect = true
+    dependence = true
 }
 
 test {
@@ -42,7 +41,14 @@ test {
         "A[2*i + 2]",
         "A[2*i + 1]"
     },
-    expect = false
+    expect = {
+        {
+            variables = Set {"i"},
+            class = "SIV",
+            coupled = false
+        }
+    },
+    dependence = false
 }
 
 test {
@@ -50,5 +56,109 @@ test {
         "A[3*i]",
         "A[2*i + 1]"
     },
-    expect = true
+    expect = {
+        {
+            variables = Set {"i"},
+            class = "SIV",
+            coupled = false
+        }
+    },
+    dependence = true
+}
+
+test {
+    input = {
+        "A[3*i][2*j - 4*k + 1]",
+        "A[2*i + 1][2*j + 4*k - 6]"
+    },
+    expect = {
+        {
+            variables = Set {"i"},
+            class = "SIV",
+            coupled = false
+        },
+        {
+            variables = Set {"j", "k"},
+            class = "MIV",
+            coupled = false
+        }
+    },
+    dependence = false
+}
+
+test {
+    input = {
+        "A[5][i + 1][j]",
+        "A[10][i][k]"
+    },
+    expect = {
+        {
+            variables = Set {},
+            class = "ZIV",
+            coupled = false
+        },
+        {
+            variables = Set {"i"},
+            class = "SIV",
+            coupled = false
+        },
+        {
+            variables = Set {"j", "k"},
+            class = "MIV",
+            coupled = false
+        }
+    },
+    dependence = false
+}
+
+test {
+    input = {
+        "A[i + 1][j + 2][k + 3]",
+        "A[i][j][k]"
+    },
+    expect = {
+        {
+            variables = Set {"i"},
+            class = "SIV",
+            coupled = false
+        },
+        {
+            variables = Set {"j"},
+            class = "SIV",
+            coupled = false
+        },
+        {
+            variables = Set {"k"},
+            class = "SIV",
+            coupled = false
+        }
+    },
+    dependence = true
+}
+
+-- Coupled subscripts
+
+test {
+    input = {
+        "A[i][j][j]",
+        "A[i][j][k + i]"
+    },
+    expect = {
+        {
+            variables = Set {"i"},
+            class = "SIV",
+            coupled = true
+        },
+        {
+            variables = Set {"j"},
+            class = "SIV",
+            coupled = true
+        },
+        {
+            variables = Set {"i", "j", "k"},
+            class = "MIV",
+            coupled = true
+        }
+    },
+    dependence = true
 }
