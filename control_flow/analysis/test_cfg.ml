b07fb6053de32d7b338aefe20fc48f1c9dd9488f
Andreas Prell <andreas.h.prell@gmail.com>
2020-04-05 13:20:16 +0200

    Update examples

--- control_flow/analysis/test_cfg.ml
+++ control_flow/analysis/test_cfg.ml
@@ -3,22 +3,32 @@ open Basic
 open Control_flow__Cfg
 open Control_flow__Inspect
 
-let fib_cfg_1 =
-  define_cfg
-    ~nodes:
-      Basic__Utils.(1--6)
-    ~edges:
-      (* Node 7 serves as exit node *)
-      [ (1, 2); (1, 3); (2, 7); (3, 4);
-        (4, 5); (4, 6); (5, 4); (6, 7); ]
-
-let fib_cfg_2 =
-  parse_file "basic_blocks/fib.hir"
-  |> basic_blocks
-  |> construct_cfg
-
-let () =
+let test () =
+  let fib_cfg_1 =
+    define_cfg
+      ~nodes:
+        Basic__Utils.(1--6)
+      ~edges:
+        (* Node 7 serves as exit node *)
+        [ (1, 2); (1, 3); (2, 7); (3, 4);
+          (4, 5); (4, 6); (5, 4); (6, 7); ]
+  in
+  let fib_cfg_2 =
+    parse_file "basic_blocks/fib.hir"
+    |> basic_blocks
+    |> construct_cfg
+  in
   inspect fib_cfg_1;
   (* fib_cfg_1 lacks source information *)
   assert (not (equal fib_cfg_1 fib_cfg_2));
   assert (equal fib_cfg_1 (discard_source_info fib_cfg_2))
+
+let () =
+  match Sys.argv with
+  | [| _; filename |] ->
+    parse_file filename
+    |> basic_blocks
+    |> construct_cfg
+    |> inspect
+  | _ ->
+    test ()
--- control_flow/analysis/test_dom.ml
+++ control_flow/analysis/test_dom.ml
@@ -4,27 +4,37 @@ open Control_flow__Cfg
 open Control_flow__Dom
 open Control_flow__Inspect
 
-let fib_cfg_1 =
-  define_cfg
-    ~nodes:
-      Basic__Utils.(1--6)
-    ~edges:
-      (* Node 7 serves as exit node *)
-      [ (1, 2); (1, 3); (2, 7); (3, 4);
-        (4, 5); (4, 6); (5, 4); (6, 7); ]
-
-let fib_cfg_2 =
-  parse_file "basic_blocks/fib.hir"
-  |> basic_blocks
-  |> construct_cfg
-
 let inspect_control_flow graph =
   let dom_sets = dominators graph in
   let idoms = immediate_dominators graph dom_sets in
   let back_edges = back_edges graph dom_sets in
   inspect graph ~dom_sets ~idoms ~back_edges
 
-let () =
+let test () =
+  let fib_cfg_1 =
+    define_cfg
+      ~nodes:
+        Basic__Utils.(1--6)
+      ~edges:
+        (* Node 7 serves as exit node *)
+        [ (1, 2); (1, 3); (2, 7); (3, 4);
+          (4, 5); (4, 6); (5, 4); (6, 7); ]
+  in
+  let fib_cfg_2 =
+    parse_file "basic_blocks/fib.hir"
+    |> basic_blocks
+    |> construct_cfg
+  in
   inspect_control_flow fib_cfg_1;
   (* Prints the same information *)
   inspect_control_flow fib_cfg_2
+
+let () =
+  match Sys.argv with
+  | [| _; filename |] ->
+    parse_file filename
+    |> basic_blocks
+    |> construct_cfg
+    |> inspect_control_flow
+  | _ ->
+    test ()
