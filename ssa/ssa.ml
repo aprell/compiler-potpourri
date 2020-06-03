2617a75bda9e98415b16609a29b80a22e77b55ab
Andreas Prell <andreas.h.prell@gmail.com>
2020-06-03 10:52:03 +0200

    Parameterize labels with non-local variables

--- ssa/ssa.ml
+++ ssa/ssa.ml
@@ -30,11 +30,17 @@ let parameterize_labels_block block ~variables =
 
 let parameterize_labels graph =
   let open Cfg.Node in
-  let variables = Array.map (fun { block; _ } ->
+  (* Collect all variables that are live on entry to _some_ basic block
+   * (see semi-pruned SSA form) *)
+  let variables = Array.fold_left (fun non_locals { block; _ } ->
       match block.source with
-      | Some { stmts; _ } -> stmts
-      | None -> assert (block.name = "Entry" || block.name = "Exit"); []
-    ) graph |> Array.to_list |> List.flatten |> all_variables
+      | Some { use; _ } ->
+        S.union (S.of_list use) non_locals
+      | None ->
+        assert (block.name = "Entry" || block.name = "Exit");
+        non_locals
+    ) S.empty graph
+  |> S.elements
   in
   Array.iter (fun node ->
       node.block <- parameterize_labels_block node.block ~variables
