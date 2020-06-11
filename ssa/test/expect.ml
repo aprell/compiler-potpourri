d906ee596d8011e42e1721d81a2b24143e17c00b
Andreas Prell <andreas.h.prell@gmail.com>
2020-06-11 14:09:21 +0200

    Add a few expect tests
    I didn't get ppx_expect to work properly, so this poor man's version
    will have to do for now.

---
+++ ssa/test/expect.ml
@@ -0,0 +1,35 @@
+open Control_flow
+open Graphs
+open Ssa
+
+let print_basic_blocks graph =
+  Cfg.basic_blocks graph
+  |> List.map Basic_block.to_string
+  |> List.filter (fun name -> name <> "Entry" && name <> "Exit")
+  |> String.concat "\n"
+  |> print_endline
+
+let convert_to_ssa graph =
+  let hline = "\n" ^ String.make 80 '-' ^ "\n" in
+
+  parameterize_labels graph;
+  print_basic_blocks graph;
+  print_endline hline;
+
+  rename_variables graph;
+  print_basic_blocks graph;
+  print_endline hline;
+
+  insert_phi_functions graph;
+  print_basic_blocks graph;
+  print_endline hline;
+
+  minimize_phi_functions graph;
+  print_basic_blocks graph
+
+let () =
+  graph_of_input
+    (match Sys.argv with
+     | [| _; filename |] -> filename
+     | _ -> failwith "Input file required")
+  |> convert_to_ssa
