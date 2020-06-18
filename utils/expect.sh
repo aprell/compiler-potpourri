3e07685893ced803259bd25d74674015f4183461
Andreas Prell <andreas.h.prell@gmail.com>
2020-06-19 00:26:50 +0200

    Extract test runner

---
+++ utils/expect.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+for test in ${1:-examples}/*; do
+    OUTPUT="test/$(basename ${test%.*}).out"
+    dune exec --no-print-directory test/expect.exe $test > $OUTPUT.actual
+    if git diff --no-index $OUTPUT.expect $OUTPUT.actual; then
+        rm $OUTPUT.actual
+    else
+        echo
+        read -p "Accept this output? [y/n/q] " -n1
+        echo
+        if [[ $REPLY =~ [Yy] ]]; then
+            mv $OUTPUT.actual $OUTPUT.expect
+        elif [[ $REPLY =~ [Qq] ]]; then
+            break
+        fi
+    fi
+done
