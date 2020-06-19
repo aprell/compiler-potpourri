7923feb3a896627957dc5b80568b0d8b013e5b32
Andreas Prell <andreas.h.prell@gmail.com>
2020-06-19 23:41:17 +0200

    ShellCheck test runner

--- utils/expect.sh
+++ utils/expect.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
+# ShellChecked
 
 set -eu
 set -o pipefail
 
-for test in ${1:-examples}/*; do
-    OUTPUT="test/$(basename ${test%.*}).out"
-    dune exec --no-print-directory test/expect.exe $test > $OUTPUT.actual
-    if git diff --no-index $OUTPUT.expect $OUTPUT.actual; then
-        rm $OUTPUT.actual
+for test in "${1:-examples}"/*; do
+    OUTPUT="test/$(basename "${test%.*}").out"
+    dune exec --no-print-directory test/expect.exe "$test" > "$OUTPUT.actual"
+    if git diff --no-index "$OUTPUT.expect" "$OUTPUT.actual"; then
+        rm "$OUTPUT.actual"
     else
         echo
-        read -p "Accept this output? [y/n/q] " -n1
+        read -rp "Accept this output [y/n/q]? " -n1
         echo
         if [[ $REPLY =~ [Yy] ]]; then
-            mv $OUTPUT.actual $OUTPUT.expect
+            mv "$OUTPUT.actual" "$OUTPUT.expect"
         elif [[ $REPLY =~ [Qq] ]]; then
             break
         fi
