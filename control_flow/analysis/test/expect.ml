112b0ec532a445aeb36d14fc245d9b80d75b19d0
Andreas Prell <andreas.h.prell@gmail.com>
2020-06-21 23:56:18 +0200

    Use DOT graphs in expect tests

---
+++ control_flow/analysis/test/expect.ml
@@ -0,0 +1,20 @@
+open Control_flow
+open Graphs
+
+let print_cfg = Cfg.output_dot
+
+let print_domtree graph =
+  let open Dom in
+  let _ = dominators graph in
+  let _ = immediate_dominators graph in
+  Domtree.(output_dot (create graph))
+
+let () =
+  match Sys.argv with
+  | [| _; filename |] ->
+    let graph = graph_of_input filename in
+    print_cfg graph;
+    print_newline ();
+    print_domtree graph
+  | _ ->
+    failwith "Input file required"
