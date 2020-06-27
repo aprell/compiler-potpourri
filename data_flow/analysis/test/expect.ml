fea74d53d86cf78bc41fbd0871c5cade37463888
Andreas Prell <andreas.h.prell@gmail.com>
2020-06-27 13:25:43 +0200

    Include dominators in expect tests

--- data_flow/analysis/test/expect.ml
+++ data_flow/analysis/test/expect.ml
@@ -1,11 +1,14 @@
 open Control_flow__Graphs
+open Dominators
 open Liveness
 
 let () =
   match Sys.argv with
   | [| _; filename |] ->
-    graph_of_input filename
-    |> Liveness.compute ~dump:true
-    |> ignore
+    let graph = graph_of_input filename in
+    print_endline "LIVE VARIABLES\n";
+    ignore (Liveness.compute graph ~dump:true);
+    print_endline "DOMINATORS\n";
+    ignore (Dominators.compute graph ~dump:true)
   | _ ->
     failwith "Input file required"
