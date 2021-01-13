9f945f650f66badc275851358d8ff1d0b29314e9
Andreas Prell <andreas.h.prell@gmail.com>
2021-01-13 20:50:58 +0100

    Use a hash table for data flow analysis results

--- data_flow/analysis/data_flow.ml
+++ data_flow/analysis/data_flow.ml
@@ -26,7 +26,7 @@ module type DataFlowType = sig
   include AnalysisType
   val traverse : Cfg.t -> Cfg.Node.t list
   (* Update in-set and out-set of a node *)
-  val update : Cfg.Node.t -> Set.t analysis array -> Set.t * Set.t
+  val update : Cfg.Node.t -> (string, Set.t analysis) Hashtbl.t -> Set.t * Set.t
 end
 
 module Forward_flow (A : AnalysisType) = struct
@@ -35,14 +35,15 @@ module Forward_flow (A : AnalysisType) = struct
   let traverse = Cfg.dfs_reverse_postorder
 
   let update (node : Cfg.Node.t) sets =
-    let { gen; kill; global_in; _ } = sets.(node.block.number) in
+    let find { Cfg.Node.block; _ } = Hashtbl.find sets block.name in
+    let { gen; kill; global_in; _ } = find node in
     match node.block.name with
     | "Entry" -> (global_in, Set.union gen global_in)
     | _ -> (
         assert (Cfg.NodeSet.cardinal node.pred > 0);
         let in_set = Cfg.NodeSet.fold (fun p set ->
-            A.meet sets.(p.block.number).global_out set
-          ) node.pred (sets.((Cfg.NodeSet.choose node.pred).block.number).global_out)
+            A.meet (find p).global_out set
+          ) node.pred (find (Cfg.NodeSet.choose node.pred)).global_out
         in
         let out_set = Set.(union gen (diff in_set kill)) in
         (in_set, out_set)
@@ -55,14 +56,15 @@ module Backward_flow (A : AnalysisType) = struct
   let traverse = Cfg.dfs_postorder
 
   let update (node : Cfg.Node.t) sets =
-    let { gen; kill; global_out; _ } = sets.(node.block.number) in
+    let find { Cfg.Node.block; _ } = Hashtbl.find sets block.name in
+    let { gen; kill; global_out; _ } = find node in
     match node.block.name with
     | "Exit" -> (Set.union gen global_out, global_out)
     | _ -> (
         assert (Cfg.NodeSet.cardinal node.succ > 0);
         let out_set = Cfg.NodeSet.fold (fun s set ->
-            A.meet sets.(s.block.number).global_in set
-          ) node.succ (sets.((Cfg.NodeSet.choose node.succ).block.number).global_in)
+            A.meet (find s).global_in set
+          ) node.succ (find (Cfg.NodeSet.choose node.succ)).global_in
         in
         let in_set = Set.(union gen (diff out_set kill)) in
         (in_set, out_set)
@@ -70,37 +72,43 @@ module Backward_flow (A : AnalysisType) = struct
 end
 
 module Data_flow_analysis (DF : DataFlowType) = struct
+  let compare_rows xs ys =
+    match xs, ys with
+    | "Entry" :: _, _ | _, "Exit" :: _ -> -1
+    | "Exit" :: _, _ | _, "Entry" :: _ -> 1
+    | _, _ -> Stdlib.compare xs ys
+
   let print_gen_kill sets =
     let rows =
-      Array.fold_left (fun rows { gen; kill; _ } ->
-          [ DF.Set.to_string gen;
+      Hashtbl.fold (fun name { gen; kill; _ } rows ->
+          [ name;
+            DF.Set.to_string gen;
             DF.Set.to_string kill ] :: rows
-        ) [] sets
-      |> List.rev
-      |> List.map2 (fun x y -> x :: y)
-        ("Entry" :: ("B" ^^ (1 -- (Array.length sets - 2))) @ ["Exit"])
+        ) sets []
+      |> List.sort compare_rows
     in
     print_table ~rows:([""; "gen"; "kill"] :: rows)
 
   let print_in_out sets =
     let rows =
-      Array.fold_left (fun rows { global_in; global_out; _ } ->
-          [ DF.Set.to_string global_in;
+      Hashtbl.fold (fun name { global_in; global_out; _ } rows ->
+          [ name;
+            DF.Set.to_string global_in;
             DF.Set.to_string global_out ] :: rows
-        ) [] sets
-    |> List.rev
-    |> List.map2 (fun x y -> x :: y)
-      ("Entry" :: ("B" ^^ (1 -- (Array.length sets - 2))) @ ["Exit"])
+        ) sets []
+      |> List.sort compare_rows
     in
     print_table ~rows:([""; "IN"; "OUT"] :: rows)
 
   let init graph =
-    let num_nodes = Cfg.get_order graph in
-    Array.init num_nodes (fun i -> DF.init (Cfg.get_node graph i) graph)
+    List.fold_left (fun tbl ({ Cfg.Node.block; _ } as node) ->
+        Hashtbl.add tbl block.name (DF.init node graph); tbl
+      ) (Hashtbl.create 10) (Cfg.get_nodes graph)
 
   let compute ?(dump = false) graph =
     let traversal = DF.traverse graph in
     let sets = init graph in
+    let find { Cfg.Node.block; _ } = Hashtbl.find sets block.name in
     let changed = ref true in
     let num_iter = ref 1 in
     if dump then (
@@ -115,11 +123,11 @@ module Data_flow_analysis (DF : DataFlowType) = struct
       changed := false;
       List.iter (fun node ->
           let in_set', out_set' = DF.update node sets in
-          if (not (DF.Set.equal in_set' sets.(node.block.number).global_in) ||
-              not (DF.Set.equal out_set' sets.(node.block.number).global_out)) then
+          if (not (DF.Set.equal in_set' (find node).global_in) ||
+              not (DF.Set.equal out_set' (find node).global_out)) then
             changed := true;
-          sets.(node.block.number).global_in <- in_set';
-          sets.(node.block.number).global_out <- out_set'
+          (find node).global_in <- in_set';
+          (find node).global_out <- out_set';
         ) traversal;
       if dump then (
         print_endline ("Iteration " ^ (string_of_int !num_iter) ^ ":");
