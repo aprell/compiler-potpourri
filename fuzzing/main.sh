3bc89399b7ee98ac2c1dc5e36cdb6539cc61bf3a
Andreas Prell <andreas.h.prell@gmail.com>
2023-08-26 11:36:51 +0200

    Use GNU Parallel to speed up fuzzing

--- fuzzing/main.sh
+++ fuzzing/main.sh
@@ -2,10 +2,38 @@
 
 set -u
 
-N=${1:-10}
+mode=sequential
+N=10
 
-for x in examples/*.hir; do
-    echo "Fuzzing $x"
-    ./mutate.sh "$x" "1-$N" > /dev/null
-    ./fuzz.sh "$x"
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -parallel|--parallel)
+      mode=parallel
+      shift
+      ;;
+    -*)
+      echo "Unknown option '$1'"
+      exit 1
+      ;;
+    *)
+      N="$1"
+      break
+      ;;
+  esac
 done
+
+if [ "$mode" = sequential ]; then
+    for x in examples/*.hir; do
+        echo "Fuzzing $x"
+        ./mutate.sh "$x" "1-$N" > /dev/null
+        ./fuzz.sh "$x"
+    done
+fi
+
+if [ "$mode" = parallel ]; then
+    parallel --jobs 8 --keep-order \
+        "echo Fuzzing {};" \
+        "./mutate.sh {} 1-$N > /dev/null;" \
+        "./fuzz.sh {}" \
+        ::: examples/*.hir
+fi
