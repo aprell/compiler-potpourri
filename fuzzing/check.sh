1472d1186aab5105e304d82aede0d2f7fdaea438
Andreas Prell <andreas.h.prell@gmail.com>
2025-03-16 20:23:18 +0100

    Replace `llc` with `opt -S`

--- fuzzing/check.sh
+++ fuzzing/check.sh
@@ -5,8 +5,8 @@ set -eu
 test="${1%.*}"
 
 if dune exec ./opt.exe "$1" > "$test.ll"; then
-    # Check LLVM IR
-    llc < "$test.ll" > /dev/null
+    # Validate LLVM IR
+    opt -S "$test.ll" > /dev/null
     # Validate transformations
     dune exec ./opt.exe -- -O "$1" > "$test.opt.ll"
     alive-tv "$test.ll" "$test.opt.ll"
