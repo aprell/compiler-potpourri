bbbcf3a9e42d97993663452724f8ee75d423662c
Andreas Prell <andreas.h.prell@gmail.com>
2026-03-08 11:44:47 +0100

    Store control variables

--- data_dependences/iteration_vector.lua
+++ data_dependences/iteration_vector.lua
@@ -31,26 +31,40 @@ local class_mt = {
 
 setmetatable(IterationVector, class_mt)
 
+-- Detect the control variables of for loops
+local FOR_1, FOR_2, FOR_3
+
+local function probe_control_variables()
+    for _ = 1, 1 do
+        FOR_1 = debug.getlocal(1, 1)
+        FOR_2 = debug.getlocal(1, 2)
+        FOR_3 = debug.getlocal(1, 3)
+        if FOR_1 == "(for generator)" then
+            -- Pre-Lua 5.4
+            assert(FOR_2 == "(for state)")
+            assert(FOR_3 == "(for control)")
+        else
+            -- Lua 5.4
+            assert(FOR_1 == "(for state)")
+            assert(FOR_2 == "(for state)")
+            assert(FOR_3 == "(for state)")
+        end
+    end
+end
+
+probe_control_variables()
+
 function IterationVector:current()
     local t = IterationVector()
     local i = 1
     while true do
         local name = getlocal(3, i)
         if not name then break end
-        if name == "(for generator)" then
-            -- Pre-Lua 5.4
-            assert(getlocal(3, i + 1) == "(for state)")
-            assert(getlocal(3, i + 2) == "(for control)")
-            t[#t + 1] = select(2, getlocal(3, i + 3))
-            i = i + 3
-        elseif name == "(for state)" then
-            -- Lua 5.4
-            assert(getlocal(3, i + 1) == "(for state)")
-            assert(getlocal(3, i + 2) == "(for state)")
-            if (getlocal(3, i + 3) == "(for state)") then
-                -- Generic for loop
-                i = i + 1
-            end
+        if name == FOR_1 then
+            assert(getlocal(3, i + 1) == FOR_2)
+            assert(getlocal(3, i + 2) == FOR_3)
+            -- Lua 5.4 generic for loop?
+            if getlocal(3, i + 3) == FOR_2 then i = i + 1 end
             t[#t + 1] = select(2, getlocal(3, i + 3))
             i = i + 3
         end
